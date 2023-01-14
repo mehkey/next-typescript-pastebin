d60b815a438fb60cf640dc2eb6c3a7c022058f41
mehkey <87057470+mehkey@users.noreply.github.com>
2023-01-13 22:53:56 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:7999' : 'https://pastebin-incubator.firebase.com'
 }
 
 module.exports = nextConfig
